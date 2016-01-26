d6cfee6c57a27dae7acf03b8f5750dfdcb609a15
hellospiral <matthewthomascarlson@gmail.com>
2016-01-25 17:16:58 -0800

    define 'move' function that transfers files

--- new_file_copier.py
+++ new_file_copier.py
@@ -15,19 +15,44 @@ conn.execute("CREATE TABLE IF NOT EXISTS updates_info(ID INTEGER PRIMARY KEY AUT
 # Retrieve the last update from the database
 cursor = conn.execute("SELECT MAX(ID), TIMESTAMP from updates_info")
 s = str(cursor.fetchall())
-lastUpdate =  s[7:22]
+lastUpdate =  s[5:21]
 
 # Function to get the path of the directory to read from
 def getReadPath():
     global src
     src = filedialog.askdirectory()
-
+    
 # Function to get the path of the directory to copy new files to
 def getWritePath():
     global dst
     dst = filedialog.askdirectory()
 
+# Function to copy all files that have been created or updated in the last 24 hours
+def move(*args):
+    listofFiles = os.listdir(src)
+    for i, f in enumerate(listofFiles):
+        st = os.stat(src+"/"+listofFiles[i])
+        if st[ST_MTIME] > (time.time() - 86400):
+            fullPath = src + "/" + f
+            shutil.copyfile(fullPath, dst + "/" + f)
+            
+    # Dialog box to alert user that function completed sucessfully
+    messagebox.showinfo("Success", "All files created or modified in the last 24 hours have been copied to " + dst)
 
+    # Generate a timestamp for current database update
+    now = datetime.datetime.now()
+    if now.minute < 10:
+        minute = '0' + str(now.minute)
+    else:
+        minute = str(now.minute)
+    timestamp = str(now.month) + "/" +str(now.day) + "/" + str(now.year) + " " + str(now.hour) + ":" + minute
+
+    # Update database with the newest timestamp
+    conn.execute("INSERT INTO updates_info (TIMESTAMP) VALUES ('" + timestamp + "')");
+    conn.commit()
+                     
+    
+# Function to make the main window of the GUI and place buttons and label within it
 def makeWindow():
     win = Tk()
     b1 = Button(win, text = "Choose Read Folder", command=getReadPath)
@@ -41,7 +66,7 @@ def makeWindow():
 
     label.grid(row = 2, columnspan = 2, column = 0, padx = 5, pady = 5)
     b3.grid(row = 3, columnspan = 2, column = 0, padx = 5, pady = 5)
-
+    
     return win
 
 # Loop for the GUI
